539dbebc4cdcdfc19b7678d62f79ead3eb7f5832
Martin Jiřička <mjiricka@gmail.com>
2016-01-10 14:19:36 +0100

    fix: Error in function parse_color.

--- src/utils.c
+++ src/utils.c
@@ -13,7 +13,7 @@ static int hex2dec(char c) {
 	c = tolower(c);
 	if ('0' <= c && c <= '9') return (c-'0');
 	else if ('a' <= c && c <= 'f') return (c-'a')+10;
-	else return -1;
+	else return -512;
 }
 
 
@@ -22,14 +22,14 @@ static int hex2dec(char c) {
 //  PUBLIC FUNCTIONS IMPLEMENTATION
 // ******************************************************************
 
-// 0 = OK
+// 0 = OK, false otherwise
 int parse_color(char *str, struct rgb_color *c)
 {
 	if (strlen(str) != 6) {
 		return 1;
 	}
 
-	int i = 0;
+	char i = 0;
 	c->r = (hex2dec(str[i++])*16 + hex2dec(str[i++])) / 255.0;
 	c->g = (hex2dec(str[i++])*16 + hex2dec(str[i++])) / 255.0;
 	c->b = (hex2dec(str[i++])*16 + hex2dec(str[i++])) / 255.0;
